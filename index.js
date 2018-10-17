02b7b6ed7c6911d2dbdb157589c27dad41c942a5
abstract-animation-9914 <vm930@hotmail.com>
2018-10-17 13:25:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file
+function findMatching (drivers, str) {
+  const newDrivers = drivers.toLowerCase();
+  return newDrivers.filter(str => str === newDrivers);
+}
+
+//fuzzyMatch()
+function fuzzyMatch (drivers, str){
+  return drivers.filter(driver => str === driver);
+}
+
+//matchName()
+
+function matchName (drivers, str){
+  const newDriver = drivers.filter(str => str === drivers.name);
+  return newDriver;
+}
